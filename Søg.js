136f9b29ff09a0826580f660fda2d57d521106c3
Josh0104 <69515856+Josh0104@users.noreply.github.com>
2021-04-13 23:51:25 +0200

    Fandt en løsning... tror jeg
    Har skrevet nogle kommentar i koden, håber det vil hjælpe 😊

--- Søg.js
+++ Søg.js
@@ -1,47 +1,65 @@
 var advInput;
 var inputArray = [
-'Administrationen', //5
+'Administrationen', 
 'Atriumgården',
 'Auditoriet', 
 'Biografen',
 'Biologi', 
-'Fysik', //10
+'Fysik', 
 'Kemi',
 'Metalværstedet',
 'Studievejledning',
 'Træværkstedet',
-'Tårnet'] //15
+'Tårnet'] 
 
 
 
 class Søg {
 
     inputfelt(){
-    advInput = document.getElementById("listInput"); //tager id fra datalist i html filen
+     advInput = document.getElementById("listInput") //tager id fra datalist i html filen
     // for(let i = 0; inputArray.length > i; i++){ // Kører array igennem ved hjælp af loop
     // kan ikke se hvad loopet gør, men man kunne bruge det ved at indtaste i inputArray[i]
     // Dog skal man lave en if statement for, hvad plantegningen er lig med, ved ikke om det
     // er den dur du er ude efter.
-  if(advInput.value == inputArray[1]){  
 
-    planTegning = 0;  //Stuen
+    var qqqq =document.getElementById("valgList").options.length; //længden af hvor mange option der er i vores inputliste
 
-  } else if(advInput.value == inputArray[2]) {
+      for(let qqq = 0; qqq < qqqq; qqq++){
 
-    planTegning = 1  //1. sal
+      var advInputArray = document.getElementById(qqq).value; //Den her loop kører alle value af datalisten igenne
+
+      // Hvis du kigger i html filen, så kan du se at jeg har skrevet en id som er mummre i hver option value
+
+      console.log(advInputArray); // output alle datalisten
+      // Det var det nemmeste løsning jeg kunne finde indtil videre. Du kan selvfølgelig ændre navne på variablerne, der jeg bare doven til at give dem et godt navn
+        // Håber det vil hjælpe lidt :)
+      }
+
+
+      
+
+
+  // if(advInput.value == inputArray[1]){  
+
+  //   planTegning = 0;  //Stuen
+
+  // } else if(advInput.value == inputArray[2]) {
+
+  //   planTegning = 1  //1. sal
     
-    } else if(advInput.value == inputArray[3]){
+  //   } else if(advInput.value == inputArray[3]){
 
-    planTegning = 2; //2. sal
+  //   planTegning = 2; //2. sal
     
-    } else if(advInput.value == inputArray[4]){
+  //   } else if(advInput.value == inputArray[4]){
 
-    planTegning = 3; //3. sal
+  //   planTegning = 3; //3. sal
     
-    } 
+  //   } 
 
-    // }
+  //   // }
 
-    }
+     }
 
 }
