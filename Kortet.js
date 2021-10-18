d3f02b3333c2a21ebbf2bbb9bb199422225fd9f6
hjaltekiller <hjalte@bjoernstrup.net>
2021-10-18 13:26:50 +0200

    Revert "Tekst i om projektet"
    This reverts commit 06c88cd18c1ad7d6bb8f0390b5f395fa5a6d7091.

--- Kortet.js
+++ Kortet.js
@@ -11,56 +11,58 @@ var indreBygninger = [];
 var popupBox;
 
 function setup() {
-  canvasStr = 1500;
-  const canvas = createCanvas(
-    canvasStr * 0.75,
-    canvasStr * canvasHeightDiff * 0.75
-  ); //canvas
-  canvas.style("display", "block");
-  canvas.style("margin", "10px");
+    canvasStr = 1500;
+    const canvas = createCanvas(
+        canvasStr * 0.75,
+        canvasStr * canvasHeightDiff * 0.75
+    ); //canvas
+    canvas.style("display", "block");
+    canvas.style("margin", "10px");
 
-  canvas.style("width", "calc(100vw - 40px)");
-  canvas.style("height", "calc(100vw * 0.4933 - 20px)");
+    canvas.style("width", "calc(100vw - 40px)");
+    canvas.style("height", "calc(100vw * 0.4933 - 20px)");
 
-  if (windowWidth < 850) {
-    canvas.style("width", "calc(850px - 20px)");
-    canvas.style("height", "calc(850px * 0.4933 - 20px");
-  }
+    if (windowWidth < 850) {
+        canvas.style("width", "calc(850px - 20px)");
+        canvas.style("height", "calc(850px * 0.4933 - 20px");
+    }
 
-  for (let i = 0; i <= 5; i++) {
-    //tilføj en figur
-    figurer[i] = new YdreBygning(20); //personlig countdown lav en ny figur, uden argument for ingen countdown
-  }
+    for (let i = 0; i <= 5; i++) {
+        //tilføj en figur
+        figurer[i] = new YdreBygning(20); //personlig countdown lav en ny figur, uden argument for ingen countdown
+    }
 
-  popupBox = new PopupBox();
-  ydreTing = new YdreBygning();
-  indreTing = new IndreBygning();
-  arraySetup = new Arrays();
-  indreBygninger = [
-    indreTing.stue,
-    indreTing.sal1,
-    indreTing.sal2,
-    indreTing.sal3,
-  ];
-  søgefelt = new Søg();
+    popupBox = new PopupBox();
+    ydreTing = new YdreBygning();
+    indreTing = new IndreBygning();
+    arraySetup = new Arrays();
+    indreBygninger = [
+        indreTing.stue,
+        indreTing.sal1,
+        indreTing.sal2,
+        indreTing.sal3,
+    ];
+    søgefelt = new Søg();
 }
 
 function draw() {
-  if (
-    popupBoxContainer.style.display == "" &&
-    hjælpereNavigation.style.display == ""
-  ) {
-    clear();
-    canvasZoom = screen.width;
-    if (planTegning != undefined) {
-      indreTing.indreBygningCanvas();
-    } else {
-      ydreTing.ydreBygningCanvas();
-    }
+    if (
+        popupBoxContainer.style.display == "" &&
+        hjælpereNavigation.style.display == ""
+    ) {
+        clear();
+        canvasZoom = screen.width;
+        if (planTegning != undefined) {
+            indreTing.indreBygningCanvas();
+        } else {
+            ydreTing.ydreBygningCanvas();
+        }
 
-    søgefelt.inputfelt();
-  }
-  popupBox.loop();
+        søgefelt.inputfelt();
+    }
+    popupBox.loop();
+
+    
 }
 
 // n =  new Date();
@@ -70,10 +72,10 @@ function draw() {
 // document.getElementById("update").innerHTML = m + "/" + d + "/" + y;
 
 function preload() {
-  kortImg = loadImage("img/sukkertoppen.png"); //kortet
-  stueImg = loadImage("img/stue.png"); //stue
-  sal1Img = loadImage("img/1.sal.png"); //1.sal
-  sal2Img = loadImage("img/2.sal.png"); //2.sal
-  sal3Img = loadImage("img/3.sal.png"); //3.sal
-  planTegningImg = loadImage("img/plantegningIcon.png");
+    kortImg = loadImage("img/sukkertoppen.png"); //kortet
+    stueImg = loadImage("img/stue.png"); //stue
+    sal1Img = loadImage("img/1.sal.png"); //1.sal
+    sal2Img = loadImage("img/2.sal.png"); //2.sal
+    sal3Img = loadImage("img/3.sal.png"); //3.sal
+    planTegningImg = loadImage("img/plantegningIcon.png");
 }
