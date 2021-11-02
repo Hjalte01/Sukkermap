7534c690a3980bce4cf6e781b2e5baa858380c3e
Josh0104 <69515856+Josh0104@users.noreply.github.com>
2021-11-02 23:48:23 +0100

    Lavet en form i kontakt os

--- html/TypeWriter.js
+++ html/TypeWriter.js
@@ -2,7 +2,7 @@
 var i = 0;
 var ii = 0;
 var txt = "Kontakt os via vores email:";
-var emailTxt = "sukkermapnext@gmail.com";
+var emailTxt = " sukkermapnext@gmail.com";
 var speed = 30;
 
 window.onload = function typeWriter() {
